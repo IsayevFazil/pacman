46ab33812abdae932d08d571f6ab543d7aafd381
Fazil Isayev <fazil.isayev.1992@mail.ru>
2015-08-03 09:36:29 +0400

    Isayev

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+ 
+public class Game : MonoBehaviour
+{
+
+    // набранные очки
+    public static int points;
+ 
+    private string _pointsString;
+    private int _lastPonts = -1;
+ 
+    // генерируем уровень при загрузке сцены
+    public void Awake()
+    {
+        // обнуляем очки
+        points = 0;
+    }
+ 
+    public void Update()
+    {
+        // обновление отображаемого текста очков только при их изменении
+        if (_lastPonts == points) return;
+ 
+        _lastPonts = points;
+        // форматируем очки в формате четырех цифр, начинающихся с нулей
+        _pointsString = "Score: "+ points.ToString("0000");
+    }
+ 
+ 
+    // отрисовка набранных очков
+    public void OnGUI()
+    {
+        GUI.color = Color.yellow;
+        GUI.Label(new Rect(100, 0, 200, 20), _pointsString ?? "");
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public int score = 0;
+
+    public int plusScore = 10;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 60, 50), "Начать\n снова"))
+        {
+            print("Start game");
+            Application.LoadLevel("scene");
+
+        }
+
+        if (GUI.Button(new Rect(10, 80, 60, 50), "Выход"))
+        {
+            print("Start game");
+            Application.Quit();
+
+        }
+
+
+    }
+}
